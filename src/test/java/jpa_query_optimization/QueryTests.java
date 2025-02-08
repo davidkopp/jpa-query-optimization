db2d12b2b575c1dc408028cb84031be61f5441d0
David Kopp <git@davidkopp.de>
2025-02-08 19:36:58 +0100

    Ensure cache of entity manager is cleared

--- src/test/java/jpa_query_optimization/QueryTests.java
+++ src/test/java/jpa_query_optimization/QueryTests.java
@@ -3,7 +3,9 @@ package jpa_query_optimization;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
-import org.junit.jupiter.api.AfterEach;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -13,20 +15,23 @@ import java.util.List;
 
 public class QueryTests {
 
-    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa_query_optimization");
-    private EntityManager em = emf.createEntityManager();
+    private static EntityManagerFactory emf;
+    private static EntityManager em;
 
     private static final int NUMBER_OF_DEPARTMENTS = 50;
     private static final int NUMBER_OF_EMPLOYEES_PER_DEPARTMENT = 3;
 
-    @BeforeEach
-    public void setUp() {
+    @BeforeAll
+    public static void fillDatabase() {
+        emf = Persistence.createEntityManagerFactory("jpa_query_optimization");
+        em = emf.createEntityManager();
+
         em.getTransaction().begin();
 
         for (int i = 0; i < NUMBER_OF_DEPARTMENTS; i++) {
             Department dept = new Department();
             dept.setName("Department " + i);
-            
+
             for (int j = 0; j < NUMBER_OF_EMPLOYEES_PER_DEPARTMENT; j++) {
                 Employee emp = new Employee();
                 emp.setName("Employee " + i + "_" + j);
@@ -39,8 +44,15 @@ public class QueryTests {
         em.getTransaction().commit();
     }
 
-    @AfterEach
-    public void tearDown() {
+    @BeforeEach
+    public void clearCache() {
+        // Important to clear the persistence context so the associated cache with
+        // cached objects is cleared
+        em.clear();
+    }
+
+    @AfterAll
+    public static void tearDown() {
         if (em != null) {
             em.close();
         }
